4b78e15ee75b90600e2fc686eaa80ae5a3055d7c
xinchen2017 <912068347@qq.com>
2018-03-14 13:37:02 +0800

    前端静态页面

--- src/main/java/com/bootdo/thesisMamager/controller/api/TeacherController.java
+++ src/main/java/com/bootdo/thesisMamager/controller/api/TeacherController.java
@@ -76,7 +76,9 @@ public class TeacherController {
 							  @ApiParam(value = "学校id", required = false) @RequestParam(required = false) Long schoolId,
 							  @ApiParam(value = "院系id", required = false) @RequestParam(required = false) Long depId,
 							  @ApiParam(value = "专业", required = false) @RequestParam(required = false) String major,
-							  @ApiParam(value = "手机号", required = false) @RequestParam(required = false) String mobile){
+								 @ApiParam(value = "职称", required = false) @RequestParam(required = false) String teacherTitle,
+								 @ApiParam(value = "邀请码", required = false) @RequestParam(required = false) String inviteCode,
+								 @ApiParam(value = "手机号", required = false) @RequestParam(required = false) String mobile){
 			AjaxResponse ajaxResponse=new AjaxResponse();
 			ThesisTeacherDO thesisTeacherDO=new ThesisTeacherDO();
 			thesisTeacherDO.setName(name);
@@ -84,192 +86,15 @@ public class TeacherController {
 			thesisTeacherDO.setSchoolId(schoolId);
 			thesisTeacherDO.setDepId(depId);
 			thesisTeacherDO.setMajor(major);
+			thesisTeacherDO.setTeacherTitle(teacherTitle);
 			thesisTeacherDO.setMobile(mobile);
 			thesisTeacherDO.setState("0");
-			MealManageDO mealManageDO=new MealManageDO();
 			if(thesisTeacherService.save(thesisTeacherDO)<=0){
 				ajaxResponse.setSuccess(false);
-				ajaxResponse.setErrorCode("失败");
+				ajaxResponse.setMsg("注册失败");
 				return ajaxResponse;
-			}else{
-				mealManageDO.setUserId(thesisTeacherDO.getId());
-				mealManageDO.setType("0");
-				mealManageService.save(mealManageDO);
 			}
 		return ajaxResponse;
 	}
-
-	@ResponseBody
-	@ApiOperation(value = "学生信息", notes = "学生信息", position = 1)
-	@RequestMapping(value = "/getone", method = {RequestMethod.POST,RequestMethod.GET})
-	public AjaxResponse login(@ApiParam(value = "id", required = false) @RequestParam(required = false) Long id){
-		AjaxResponse ajaxResponse=new AjaxResponse();
-		Map map=new HashMap();
-		map.put("id",id);
-		ThesisStudentDO thesisStudentDO=thesisStudentService.queryone(map);
-		ThesisCollegeDO thesisCollegeDO=thesisCollegeService.get(thesisStudentDO.getDepId());
-		thesisStudentDO.setDepName(thesisCollegeDO.getName());
-		if(thesisStudentDO ==null){
-			ajaxResponse.setSuccess(false);
-			ajaxResponse.setMsg("操作失败");
-			return ajaxResponse;
-		}else{
-			ajaxResponse.setResult(thesisStudentDO);
-		}
-		return ajaxResponse;
-	}
-
-	@ResponseBody
-	@ApiOperation(value = "学生登陆", notes = "学生登陆", position = 1)
-	@RequestMapping(value = "/login", method = {RequestMethod.POST,RequestMethod.GET})
-	public AjaxResponse login(@ApiParam(value = "密码", required = false) @RequestParam(required = false) String passWord,
-							  @ApiParam(value = "手机号", required = false) @RequestParam(required = false) String mobile){
-		AjaxResponse ajaxResponse=new AjaxResponse();
-		Map map=new HashMap();
-		map.put("mobile",mobile);
-		map.put("passWord",MD5Utils.encrypt(mobile, passWord));
-		ThesisStudentDO thesisStudentDO=thesisStudentService.queryone(map);
-		if(thesisStudentDO ==null){
-			ajaxResponse.setSuccess(false);
-			ajaxResponse.setMsg("操作失败");
-			return ajaxResponse;
-		}else{
-			ajaxResponse.setResult(thesisStudentDO);
-		}
-		return ajaxResponse;
-	}
-
-	@ResponseBody
-	@ApiOperation(value = "学生手机唯一性", notes = "学生手机唯一性", position = 1)
-	@RequestMapping(value = "/checkmobile", method = {RequestMethod.POST,RequestMethod.GET})
-	public AjaxResponse checkmobile(@ApiParam(value = "手机号", required = false) @RequestParam(required = false) String mobile){
-		AjaxResponse ajaxResponse=new AjaxResponse();
-		Map map=new HashMap();
-		map.put("mobile",mobile);
-		ThesisStudentDO thesisStudentDO=thesisStudentService.queryone(map);
-		if(thesisStudentDO ==null){
-			ajaxResponse.setSuccess(false);
-			ajaxResponse.setMsg("操作失败");
-			return ajaxResponse;
-		}else{
-			ajaxResponse.setResult(thesisStudentDO);
-		}
-		return ajaxResponse;
-	}
-
-
-	@ResponseBody
-	@ApiOperation(value = "查询学校-院系", notes = "查询学校-院系", position = 1)
-	@RequestMapping(value = "/querySchool", method = {RequestMethod.GET})
-	public AjaxResponse querySchool(@ApiParam(value = "父id", required = false) @RequestParam(required = false) Integer pid){
-		AjaxResponse ajaxResponse=new AjaxResponse();
-		Map map=new HashMap();
-		if(pid!=null){
-			map.put("pid",pid);
-			map.put("state","0");
-			List<ThesisCollegeDO> li= thesisCollegeService.queryall(map);
-			if(li.size()<=0){
-				ajaxResponse.setSuccess(false);
-				ajaxResponse.setMsg("操作失败");
-				return ajaxResponse;
-			}else{
-				ajaxResponse.setResult(li);
-			}
-		}else{
-			map.put("pid","0");
-			map.put("state","0");
-			List<ThesisCollegeDO> li= thesisCollegeService.queryall(map);
-			if(li.size()<=0){
-				ajaxResponse.setSuccess(false);
-				ajaxResponse.setMsg("操作失败");
-				return ajaxResponse;
-			}else{
-				ajaxResponse.setResult(li);
-			}
-		}
-
-		return ajaxResponse;
-	}
-
-
-	@ResponseBody
-	@ApiOperation(value = "查询老师", notes = "查询老师", position = 1)
-	@RequestMapping(value = "/queryteacher", method = {RequestMethod.GET})
-	public AjaxResponse queryteacher(@ApiParam(value = "院系id", required = false) @RequestParam(required = false) Long depId){
-		AjaxResponse ajaxResponse=new AjaxResponse();
-		Map map=new HashMap();
-
-			map.put("depId",depId);
-			map.put("state","0");
-			List<ThesisCollegeDO> li=thesisTeacherService.list(map);
-			if(li.size()<=0){
-				ajaxResponse.setSuccess(false);
-				ajaxResponse.setMsg("操作失败");
-				return ajaxResponse;
-			}else{
-				ajaxResponse.setResult(li);
-			}
-
-		return ajaxResponse;
-	}
-
-	@ResponseBody
-	@ApiOperation(value = "查询学习层次,学习形式", notes = "type:study_type学习层次,study_way学习形式", position = 1)
-	@RequestMapping(value = "/studytype", method = {RequestMethod.GET})
-	public AjaxResponse studytype(@ApiParam(value = "院系id", required = false) @RequestParam(required = false) String type){
-		AjaxResponse ajaxResponse=new AjaxResponse();
-		Map map=new HashMap();
-		map.put("type",type);
-		List<DictDO> li=sysDictService.list(map);
-		if(li.size()<=0){
-			ajaxResponse.setSuccess(false);
-			ajaxResponse.setMsg("操作失败");
-			return ajaxResponse;
-		}else{
-			ajaxResponse.setResult(li);
-		}
-
-		return ajaxResponse;
-	}
-
-	@ResponseBody
-	@ApiOperation(value = "学生信息列表", notes = "学生信息列表", position = 1)
-	@RequestMapping(value = "/getStudentMessage", method = {RequestMethod.POST,RequestMethod.GET})
-	public AjaxResponse getStudentMessage(@ApiParam(value = "id", required = false) @RequestParam(required = false) Long id){
-		AjaxResponse ajaxResponse=new AjaxResponse();
-		Map map=new HashMap();
-		map.put("id",id);
-		ThesisStudentDO thesisStudentDO=thesisStudentService.queryone(map);
-		ThesisCollegeDO thesisCollegeDO=thesisCollegeService.get(thesisStudentDO.getDepId());
-		thesisStudentDO.setDepName(thesisCollegeDO.getName());
-		if(thesisStudentDO ==null){
-			ajaxResponse.setSuccess(false);
-			ajaxResponse.setMsg("操作失败");
-			return ajaxResponse;
-		}else{
-			ajaxResponse.setResult(thesisStudentDO);
-		}
-		return ajaxResponse;
-	}
-
-
-
-	@ResponseBody
-	@GetMapping("/Studentlist")
-	public AjaxResponse list(@ApiParam(value = "页码", required = false) @RequestParam(required = false) Integer page,
-							 @ApiParam(value = "每页条数", required = false) @RequestParam(required = false) Integer limit){
-		PageHelper.startPage(page, limit);
-		//查询列表数据
-        //Query query = new Query(params);
-		Map query=new HashMap();
-		query.put("state",1);
-		List<ThesisStudentDO> thesisStudentList = thesisStudentService.list(query);
-		for (ThesisStudentDO li:thesisStudentList) {
-			ThesisCollegeDO thesisCollegeDO=thesisCollegeService.get(li.getDepId());
-			li.setDepName(thesisCollegeDO.getName());
-		}
-		return new AjaxResponse(thesisStudentList);
-	}
-
 
 }
